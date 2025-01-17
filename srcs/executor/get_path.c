2fb8abf3b72302f9091b023586460bb76e01cf88
Jeremy Le Nezet <jle-neze@bess-f3r5s13.clusters.42paris.fr>
2025-01-17 12:32:44 +0100

    norminette2 getpath

--- srcs/executor/get_path.c
+++ srcs/executor/get_path.c
@@ -10,8 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../include/minishell.h"
-
 char	*ft_getenv_dup(char *path, t_env *env_list)
 {
 	if (!path)
@@ -30,9 +28,7 @@ char	*ft_getenv_dup(char *path, t_env *env_list)
 char	*ft_getenv(char *path, t_env *env_list)
 {
 	if (!path)
-	{
 		return (NULL);
-	}
 	while (env_list)
 	{
 		if (ft_strcmp(env_list->name, path) == 0)
@@ -42,38 +38,51 @@ char	*ft_getenv(char *path, t_env *env_list)
 	return (NULL);
 }
 
+int	init_path(char **path_var, char ***path, t_env *env_list)
+{
+	*path_var = NULL;
+	*path = NULL;
+	*path_var = ft_getenv("PATH", env_list);
+	if (!*path_var)
+		return (0);
+	*path = ft_split(*path_var, ':');
+	if (!*path)
+		return (0);
+	return (1);
+}
+
+char	*init_exec(char **path, char **path_part, char *cmd, int i)
+{
+	char	*exec;
+
+	*path_part = NULL;
+	exec = NULL;
+	*path_part = ft_strjoin(path[i], "/");
+	if (!*path_part)
+		return (NULL);
+	exec = ft_strjoin(*path_part, cmd);
+	if (!exec)
+		return (NULL);
+	return (exec);
+}
+
 char	*get_cmd_path(char *cmd, t_env *env_list)
 {
 	int		i;
-	char	*exec;
-	char	*path_part;
-	char	**path;
 	char	*path_var;
+	char	**path;
+	char	*path_part;
+	char	*exec;
 
 	if (!cmd)
 		return (NULL);
-	path_var = ft_getenv("PATH", env_list);
-	if (!path_var)
-		return (NULL);
-	path = ft_split(path_var, ':');
-	if (!path)
+	if (init_path(&path_var, &path, env_list) == 0)
 		return (NULL);
 	i = -1;
 	while (path[++i])
 	{
-		path_part = ft_strjoin(path[i], "/");
-		if (!path_part)
-		{
-			free_tab(path);
-			return (NULL);
-		}
-		exec = ft_strjoin(path_part, cmd);
+		exec = init_exec(path, &path_part, cmd, i);
 		free(path_part);
-		if (!exec)
-		{
-			free_tab(path);
-			return (NULL);
-		}
 		if (access(exec, F_OK | X_OK) == 0)
 		{
 			free_tab(path);
